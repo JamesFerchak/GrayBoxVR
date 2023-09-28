495f4b15d82b3706dbbf4d895335ef447fd0912e
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2023-09-28 12:57:39 -0400

    Button debug
    debog log now detects button presses

--- Assets/Player Rig/Scripts/LeftHandController.cs
+++ Assets/Player Rig/Scripts/LeftHandController.cs
@@ -1,18 +1,45 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.OpenVR;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class LeftHandController : MonoBehaviour
 {
+    //  -------------------------------------------------------------------------------------------------------------------  
+    // Input references
+    public InputActionReference xButton = null; // reference to the A button action in the input map.
+    public InputActionReference lTrigger = null; // reference to the trigger action in the input map.
+    public InputActionReference yButton = null;
+    public InputActionReference lGrip = null;
+
+    //  -------------------------------------------------------------------------------------------------------------------  
     // Start is called before the first frame update
-    void Start()
+    public void Awake() // tutorial used awake so I did as well. Not sure yet if start changes how it works at all.
     {
-        
+        xButton.action.started += xToggle; // How the a button is gets its pressed detected.
+        yButton.action.started += yToggle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float tValue = lTrigger.action.ReadValue<float>();
+        float gValue = lGrip.action.ReadValue<float>();
+        if (tValue > 0 || gValue > 0)
+        {
+            Debug.Log("Left \nTrigger Value = " + tValue + "\n" + "Grip Value = " + gValue);
+        }
+    }
+
+    public void xToggle(InputAction.CallbackContext context)
+    {
+        Debug.Log("X button pressed.");
+    }
+    public void yToggle(InputAction.CallbackContext context)
+    {
+        Debug.Log("Y button pressed.");
     }
 }
--- Assets/Player Rig/Scripts/RightHandController.cs
+++ Assets/Player Rig/Scripts/RightHandController.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; 
+using UnityEngine.UI;
 
 public class RightHandController : MonoBehaviour
 {
+//  -------------------------------------------------------------------------------------------------------------------  
+    // Input references
+    public InputActionReference aButton = null; // reference to the A button action in the input map.
+    public InputActionReference rTrigger = null; // reference to the trigger action in the input map.
+    public InputActionReference bButton = null;
+    public InputActionReference rGrip = null;
+
+//  -------------------------------------------------------------------------------------------------------------------  
     // Start is called before the first frame update
-    void Start()
+    public void Awake() // tutorial used awake so I did as well. Not sure yet if start changes how it works at all.
     {
-        
+        aButton.action.started += aToggle; // How the a button is gets its pressed detected.
+        bButton.action.started += bToggle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float tValue = rTrigger.action.ReadValue<float>();
+        float gValue = rGrip.action.ReadValue<float>();
+        if (tValue > 0 || gValue > 0)
+        {
+            Debug.Log("Right: \nTrigger Value = " + tValue + "\n" + "Grip Value = " + gValue);
+        }
+    }
+
+    public void aToggle(InputAction.CallbackContext context)
+    {
+        Debug.Log("A button pressed.");
+    }
+    public void bToggle(InputAction.CallbackContext context)
+    {
+        Debug.Log("B button pressed.");
     }
 }
