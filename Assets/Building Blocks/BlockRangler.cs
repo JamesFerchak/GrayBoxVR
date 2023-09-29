dcdca20a6a290092b87acaab92220a3ff101761d
Vinni Horstkotte <64919019+Rin1ton@users.noreply.github.com>
2023-09-29 15:52:10 -0400

    Make Block rangler, and resources/blocks folder

---
+++ Assets/Building Blocks/BlockRangler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BlockRangler : MonoBehaviour
+{
+    private static BlockRangler _singleton;
+    public static BlockRangler Singleton
+    {
+        get => _singleton;
+        private set
+        {
+            if (_singleton == null) _singleton = value;
+            else
+            {
+                Debug.LogWarning($"There is more than one Block Rangler! Killing self!!!");
+                Destroy(value.gameObject);
+            }
+        }
+    }
+
+	private void Awake()
+	{
+        Singleton = this;
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
