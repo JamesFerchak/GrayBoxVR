d4294865754afba60ad2cb6d41deb6b4def624d8
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2023-10-10 15:10:32 -0400

    fix rig prefab

--- Assets/Player Rig/Scripts/ObjectManipulator.cs
+++ Assets/Player Rig/Scripts/ObjectManipulator.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ObjectManipulator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void TryGrab(float grabValue)
+	{
+		Debug.LogWarning($"I am grabbing!!! {grabValue}");
+	}
 }
