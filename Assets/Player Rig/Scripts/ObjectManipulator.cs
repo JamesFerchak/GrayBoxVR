43c15da585775def65b4bdc3e38c568c14f4c367
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2023-10-17 14:13:40 -0400

    grabbing and moving objects **should** work
    idk tho, I couldn't test it

--- Assets/Player Rig/Scripts/ObjectManipulator.cs
+++ Assets/Player Rig/Scripts/ObjectManipulator.cs
@@ -10,6 +10,9 @@ public class ObjectManipulator : MonoBehaviour
 	readonly float grabTreshhold = 0.8f;
 
 	private GameObject currentlyHeldObject = null;
+	bool triedToGrabAlready = false;
+	Vector3 controllerStartingPosition = Vector3.zero;
+	Vector3 objectStartingPosition = Vector3.zero;
 
 	// Start is called before the first frame update
 	void Start()
@@ -20,34 +23,71 @@ public class ObjectManipulator : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		
+		MoveGrabbedObject();
 	}
 
 	public void TryGrab(float grabValue)
 	{
-		List<Collider> possibleColliders = new(Physics.OverlapSphere(cursor.transform.position, .05f));
-		float distanceToNearest = Mathf.Infinity;
-		int indexOfNearest = -1;
 
-		//go through list of colliders and 
-		foreach (Collider collider in possibleColliders)
+		//try to grab
+		if (currentlyHeldObject == null && grabValue >= grabTreshhold && !triedToGrabAlready)
 		{
-			if (collider.gameObject.tag == "Block")
+			//mark that we already tried to grab
+			triedToGrabAlready = true;
+
+			//create list of colliders
+			List<Collider> possibleColliders = new(Physics.OverlapSphere(cursor.transform.position, .05f));
+
+			//get ready to go through list
+			float distanceToNearest = Mathf.Infinity;
+			int indexOfNearest = -1;
+
+			//go through list of near colliders and find the closest one with the Block tag
+			foreach (Collider collider in possibleColliders)
 			{
-				float distanceToThis = Vector3.Distance(cursor.transform.position, collider.transform.position);
-				if (distanceToThis < distanceToNearest)
+				if (collider.gameObject.tag == "Block")
 				{
-					distanceToNearest = distanceToThis;
-					indexOfNearest = possibleColliders.IndexOf(collider);
-				}
-				else
-				{
-					possibleColliders.Remove(collider);
+					float distanceToThis = Vector3.Distance(cursor.transform.position, collider.transform.position);
+					if (distanceToThis < distanceToNearest)
+					{
+						distanceToNearest = distanceToThis;
+						indexOfNearest = possibleColliders.IndexOf(collider);
+					}
+					else
+					{
+						possibleColliders.Remove(collider);
+					}
 				}
 			}
+
+			//if there is a viable collider, make it the grabbed object
+			if (indexOfNearest >= 0 && indexOfNearest < possibleColliders.Count)
+			{
+				if (possibleColliders[indexOfNearest] != null)
+				{
+					currentlyHeldObject = possibleColliders[indexOfNearest].gameObject;
+					controllerStartingPosition = transform.position;
+					objectStartingPosition = currentlyHeldObject.transform.position;
+				}
+
+			}
+		}
+		//if we're holing something and we aren't holding the grab button, ungrab
+		else if (currentlyHeldObject != null && grabValue < grabTreshhold)
+		{
+			currentlyHeldObject = null;
 		}
 
-		if (possibleColliders[indexOfNearest] != null)
-			currentlyHeldObject = possibleColliders[indexOfNearest].gameObject;
+		//if we've ungrabbed, get ready to try again
+		if (grabValue < grabTreshhold) triedToGrabAlready = false;
+	}
+
+	private void MoveGrabbedObject()
+	{
+		if (currentlyHeldObject != null)
+		{
+			Vector3 positionToAdd = transform.position - controllerStartingPosition;
+			currentlyHeldObject.transform.position = objectStartingPosition + positionToAdd;
+		}
 	}
 }
