ba4cce73f0251eb7ef9efe5657dffa74b4eda60a
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2023-10-17 13:31:45 -0400

    got rid of collider on rig; started grabbing
    now using Physics.OverlapShere to determine grabbed objects.

--- Assets/Player Rig/Scripts/ObjectManipulator.cs
+++ Assets/Player Rig/Scripts/ObjectManipulator.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectManipulator : MonoBehaviour
 {
+
+	[SerializeField] GameObject cursor;
+	readonly float grabTreshhold = 0.8f;
+
+	private GameObject currentlyHeldObject = null;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -18,6 +25,29 @@ public class ObjectManipulator : MonoBehaviour
 
 	public void TryGrab(float grabValue)
 	{
-		Debug.LogWarning($"I am grabbing!!! {grabValue}");
+		List<Collider> possibleColliders = new(Physics.OverlapSphere(cursor.transform.position, .05f));
+		float distanceToNearest = Mathf.Infinity;
+		int indexOfNearest = -1;
+
+		//go through list of colliders and 
+		foreach (Collider collider in possibleColliders)
+		{
+			if (collider.gameObject.tag == "Block")
+			{
+				float distanceToThis = Vector3.Distance(cursor.transform.position, collider.transform.position);
+				if (distanceToThis < distanceToNearest)
+				{
+					distanceToNearest = distanceToThis;
+					indexOfNearest = possibleColliders.IndexOf(collider);
+				}
+				else
+				{
+					possibleColliders.Remove(collider);
+				}
+			}
+		}
+
+		if (possibleColliders[indexOfNearest] != null)
+			currentlyHeldObject = possibleColliders[indexOfNearest].gameObject;
 	}
 }
