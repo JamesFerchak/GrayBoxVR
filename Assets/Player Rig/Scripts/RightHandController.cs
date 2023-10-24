91792d2ba2e7483f4ca1d5893207d5e55223184c
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2023-10-24 12:58:46 -0400

    fixed teleport error

--- Assets/Player Rig/Scripts/RightHandController.cs
+++ Assets/Player Rig/Scripts/RightHandController.cs
@@ -64,11 +64,6 @@ public class RightHandController : MonoBehaviour
         {
             Debug.Log("Right: \nTrigger Value = " + tValue + "\n" + "Grip Value = " + gValue);
         }
-        
-        if (svalue.y > .8 && svalue.y <= 1)
-        {
-            Teleport();
-        }
 
         myOM.TryGrab(gValue);
     }
