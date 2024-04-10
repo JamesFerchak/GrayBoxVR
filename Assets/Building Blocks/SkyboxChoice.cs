d8f417aac93c916596b600cfab7a32c8dc86c574
PaulAntuono <ptantuono@yahoo.com>
2024-04-10 14:56:48 -0400

    skybox code and tablecube

--- Assets/Building Blocks/SkyboxChoice.cs
+++ Assets/Building Blocks/SkyboxChoice.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class SkyboxChoice : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static SkyboxChoice _singleton;
+    public static SkyboxChoice Singleton
     {
-        
+        get => _singleton;
+        private set
+        {
+            if (_singleton == null) _singleton = value;
+            else
+            {
+                Debug.LogWarning($"There is more than one sky box choice! Killing self!!!");
+                Destroy(value.gameObject);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Material> skyboxMaterials = new List<Material>();
+
+    
+    public void ChangeSkybox(int skyboxIndex)
     {
-        
+        if (skyboxIndex >= 0 && skyboxIndex < skyboxMaterials.Count)
+        {
+            //Debug.LogWarning("trying to change skybox!!!.");
+            RenderSettings.skybox = skyboxMaterials[skyboxIndex];
+            
+            DynamicGI.UpdateEnvironment();
+        }
+        else
+        {
+            Debug.LogWarning("invalid index.");
+        }
+    }
+
+    void Start()
+    {
+        ChangeSkybox(2);
     }
 }
